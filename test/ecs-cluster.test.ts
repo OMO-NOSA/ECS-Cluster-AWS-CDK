44d490f3ba8755d86ea377fcc3de27a671766e73
nosa omorodion <nosaomorodion@surfer-172-30-5-220-hotspot.internet-for-guests.com>
2022-02-08 21:59:06 +0100

    Initial commit

---
+++ test/ecs-cluster.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsCluster from '../lib/ecs-cluster-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-cluster-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsCluster.EcsClusterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
